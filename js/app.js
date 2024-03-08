32b0e9dd4236a150d445758803c33a35089b4159
MattiaBologna <mattiabologna01@gmail.com>
2024-03-08 15:12:40 +0100

    Add: static markup

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// MILESTONE 1
+// Per prima cosa, creiamo il markup statico: 
+// costruiamo il container e inseriamo un'immagine grande al centro: 
+// avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
